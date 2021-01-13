0d7f4643f2592d4d007d2cd9a1443b62226f79f1
b737m <65369608+b737m@users.noreply.github.com>
2021-01-12 20:05:42 -0500

    Add

---
+++ main.js
@@ -0,0 +1,146 @@
+function vid() {
+  Swal.fire({
+    title: "<strong>Fire Mitigation</strong>",
+    html:
+      '<video width="100%" controls><source src="https://firebasestorage.googleapis.com/v0/b/expteam7projwildfire.appspot.com/o/Master.0000_1.mp4?alt=media&token=5138a5e2-e7b3-418e-ba6b-d5b96f178d4c" type="video/mp4"> <source src="./Master.0000_1.mp4" type="audio/mp4"></video>',
+    customClass: "swal-wide",
+  });
+}
+
+var hi = `<ul>
+<li>Ellen Gray, NASA’s Earth Science News Team, “Satellite Data Record Shows Climate’s Impact on Fires,” September 10, 2019 (accessed January 2021).<a href="https://climate.nasa.gov/news/2912/satellite-data-record-shows-climate-changes-impact-on-fires/ "><button class="btn btn-outline-info"> NASA</button></a></li>
+<br>
+<br>
+<li>Alejandra Borunda, “The Science Connecting Wildfires to Climate Change.” (accessed January 2021)<a href="https://www.nationalgeographic.com/science/2020/09/climate-change-increases-risk-fires-western-us/"><button class="btn btn-outline-info"> Alejandra Borunda</button></a></li>
+<br>
+<br>
+<li>Union of Concerned Scientists, “Infographic: Wildfires and Climate Change, Visualizing the Connection in Five Sets of Photos and Charts”, published September 8, 2020 (accessed January 2021).<a href="ucsusa.org/resources/infographic-wildfires-and-climate-change"><button class="btn btn-outline-info"> Union of Concerned Scientists</button></a></li>
+<br>
+<br>
+<li>United States Environmental Protection Agency, “Climate Change Indicators: Wildfires”, (accessed January 2021)<a href="https://www.epa.gov/climate-indicators/climate-change-indicators-wildfires"><button class="btn btn-outline-info">United States Environmental Protection Agency</button></a></li>
+<br>
+<br>
+<li>National Ecological Observatory Network (NEON) Science, "How Does LiDAR Remote Sensing Work? Light Detection and Ranging", November 24, 2014 (accessed January 2021).<a href="https://youtu.be/EYbhNSUnIdU"><button class="btn btn-outline-info">NEON</button></a></li>
+<br>
+<br>
+<li>"How Scientists are Trying to Predict Wildfire Movement", PBS NewsHour, PBS, May 8, 2019. (Accessed January 2021)<a href="https://www.youtube.com/watch?v=PNv_76qAgBw&feature=emb_logo"><button class="btn btn-outline-info">PBS</button></a></li>
+<br>
+<br>
+<li>"Fire Research", U.S. Forest Service, (accessed January 2021). <a href="https://www.fs.usda.gov/science-technology/fire/fire-research"><button class="btn btn-outline-info">USFS</button></a></li>
+<br>
+<br>
+<li>United States Forest Service, “Research Highlights”, (accessed January 2021)<a href="https://www.fs.fed.us/research/highlights/highlights_display.php?in_high_id=874"><button class="btn btn-outline-info">USFS</button></a></li>
+<br>
+<br>
+<li>Larry Hardesty. (2017, April 14). Explained: Neural networks. <a href="https://news.mit.edu/2017/explained-neural-networks-deep-learning-0414"><button class="btn btn-outline-info">NEON</button></a></li>
+<br>
+<br>
+</ul>
+`
+
+function m1() {
+  Swal.fire({
+    title: "<strong>Citation</strong>",
+    html:
+    `
+    <ul>
+      <li>Ellen Gray, NASA’s Earth Science News Team, “Satellite Data Record Shows Climate’s Impact on Fires,” September 10, 2019 (accessed January 2021).<a href="https://climate.nasa.gov/news/2912/satellite-data-record-shows-climate-changes-impact-on-fires/ "><button class="btn btn-outline-info"> NASA</button></a></li>
+      <br>
+      <li>National Ecological Observatory Network (NEON) Science, "How Does LiDAR Remote Sensing Work? Light Detection and Ranging", November 24, 2014 (accessed January 2021).<a href="https://youtu.be/EYbhNSUnIdU"><button class="btn btn-outline-info">NEON</button></a></li>
+      <br>
+      <li>"Fire Research", U.S. Forest Service, (accessed January 2021). <a href="https://www.fs.usda.gov/science-technology/fire/fire-research"><button class="btn btn-outline-info">USFS</button></a></li>
+      <br>
+      <br>
+      <li>United States Forest Service, “Research Highlights”, (accessed January 2021)<a href="https://www.fs.fed.us/research/highlights/highlights_display.php?in_high_id=874"><button class="btn btn-outline-info">USFS</button></a></li>
+      <br>
+    </ul>
+  `,
+  customClass: "swal-m",
+  });
+}
+
+function m2() {
+  Swal.fire({
+    title: "<strong>Citation</strong>",
+    html:
+      `
+        <ul>
+          <li>"How Scientists are Trying to Predict Wildfire Movement", PBS NewsHour, PBS, May 8, 2019. (Accessed January 2021)<a href="https://www.youtube.com/watch?v=PNv_76qAgBw&feature=emb_logo"><button class="btn btn-outline-info">PBS</button></a></li>
+          <br>
+          <li>"Fire Research", U.S. Forest Service, (accessed January 2021). <a href="https://www.fs.usda.gov/science-technology/fire/fire-research"><button class="btn btn-outline-info">USFS</button></a></li>
+          <br>
+          <br>
+          <li>United States Forest Service, “Research Highlights”, (accessed January 2021)<a href="https://www.fs.fed.us/research/highlights/highlights_display.php?in_high_id=874"><button class="btn btn-outline-info">USFS</button></a></li>
+          <br>
+        </ul>
+      `,
+      customClass: "swal-m",
+  });
+}
+
+function m3() {
+  Swal.fire({
+    title: "<strong>Citation</strong>",
+    html:
+    `
+    <ul>
+      <li>National Ecological Observatory Network (NEON) Science, "How Does LiDAR Remote Sensing Work? Light Detection and Ranging", November 24, 2014 (accessed January 2021).<a href="https://youtu.be/EYbhNSUnIdU"><button class="btn btn-outline-info">NEON</button></a></li>
+      <br>
+      <li>"How Scientists are Trying to Predict Wildfire Movement", PBS NewsHour, PBS, May 8, 2019. (Accessed January 2021)<a href="https://www.youtube.com/watch?v=PNv_76qAgBw&feature=emb_logo"><button class="btn btn-outline-info">PBS</button></a></li>
+      <br>
+    </ul>
+  `,
+  customClass: "swal-m",
+  });
+}
+
+function m4() {
+  Swal.fire({
+    title: "<strong>Citation</strong>",
+    html:
+    `
+    <ul>
+      <li>National Ecological Observatory Network (NEON) Science, "How Does LiDAR Remote Sensing Work? Light Detection and Ranging", November 24, 2014 (accessed January 2021).<a href="https://youtu.be/EYbhNSUnIdU"><button class="btn btn-outline-info">NEON</button></a></li>
+      <br>
+      <li>"How Scientists are Trying to Predict Wildfire Movement", PBS NewsHour, PBS, May 8, 2019. (Accessed January 2021)<a href="https://www.youtube.com/watch?v=PNv_76qAgBw&feature=emb_logo"><button class="btn btn-outline-info">PBS</button></a></li>
+      <br> 
+    </ul>
+  `,
+  customClass: "swal-m",
+  });
+}
+
+function m5() {
+  Swal.fire({
+    title: "<strong>Citation</strong>",
+    html:
+    `
+    <ul>
+    <li>Larry Hardesty. (2017, April 14). Explained: Neural networks. <a href="https://news.mit.edu/2017/explained-neural-networks-deep-learning-0414"><button class="btn btn-outline-info">NEON</button></a></li>
+    <br>
+    </ul>
+  `,
+  customClass: "swal-m",
+  });
+}
+
+function m6() {
+  Swal.fire({
+    title: "<strong>Citation</strong>",
+    html:
+    `
+    <ul>
+      <li>Ellen Gray, NASA’s Earth Science News Team, “Satellite Data Record Shows Climate’s Impact on Fires,” September 10, 2019 (accessed January 2021)<a href="https://climate.nasa.gov/news/2912/satellite-data-record-shows-climate-changes-impact-on-fires/ "><button class="btn btn-outline-info"> NASA</button></a></li>
+      <br>
+      <li>National Ecological Observatory Network (NEON) Science, "How Does LiDAR Remote Sensing Work? Light Detection and Ranging", November 24, 2014 (accessed January 2021)<a href="https://youtu.be/EYbhNSUnIdU"><button class="btn btn-outline-info">NEON</button></a></li>
+      <br>
+      <li>"Fire Research", U.S. Forest Service, (accessed January 2021) <a href="https://www.fs.usda.gov/science-technology/fire/fire-research"><button class="btn btn-outline-info">USFS</button></a></li>
+      <br>
+      <br>
+      <li>United States Forest Service, “Research Highlights”, (accessed January 2021)<a href="https://www.fs.fed.us/research/highlights/highlights_display.php?in_high_id=874"><button class="btn btn-outline-info">USFS</button></a></li>
+      <br>
+    </ul>
+  `,
+  customClass: "swal-m",
+  });
+}
